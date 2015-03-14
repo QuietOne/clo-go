d672ea79e008f45187ae50044706e5d13ca18d24
QuietOne <tihomir.radosavljevic@gmail.com>
2015-03-14 17:45:51 +0100

    Functional go board

--- bin/clo_go/board.clj
+++ bin/clo_go/board.clj
@@ -34,6 +34,9 @@
 (defn empty-field? [board x y]
   (= (piece-at board x y) :empty))
 
+(defn occupied? [board x y]
+  (not (empty-field? board x y)))
+
 (defn valid-pos? [x y]
   (and
     (< x board-size)
@@ -47,91 +50,112 @@
 (defn ^:private remove-piece [board x y]
   (assoc-in board [x y] "-"))
 
+(defn ^:private liber? [board v color x y]
+  (if (not (.contains v [x y]))
+    (or
+      (or
+        (and 
+          (valid-pos? (dec x) y)
+          (empty-field? board (dec x) y))
+        (and
+          (valid-pos? (dec x) y)
+          (same-color? board color (dec x) y)
+          (liber? board (conj v [x y]) color (dec x) y)))
+      (or
+        (and
+          (valid-pos? (inc x) y)
+          (empty-field? board (inc x) y))
+        (and 
+          (valid-pos? (inc x) y)
+          (same-color? board color (inc x) y)
+          (liber? board (conj v [x y]) color (inc x) y)))
+      (or
+        (and
+          (valid-pos? x (dec y))
+          (empty-field? board x (dec y)))
+        (and           
+          (valid-pos? x (dec y))
+          (same-color? board color x (dec y))
+          (liber? board (conj v [x y]) color x (dec y))))
+      (or
+        (and
+          (valid-pos? x (inc y))
+          (empty-field? board x (inc y)))
+        (and 
+          (valid-pos? x (inc y))
+          (same-color? board color x (inc y))
+          (liber? board (conj v [x y]) color x (inc y)))))))
+
 (defn liberty? [board color x y]
-  (or
-    (or
-      (and
-        (valid-pos? (dec x) y)
-        (empty-field? board (dec x) y))
-      (if (same-color? board color (dec x) y)
-        (liberty? board color (dec x) y)
-        false))
-    (or
-      (and
-        (valid-pos? (inc x) y)
-        (empty-field? board (inc x) y))
-      (if (same-color? board color (inc x) y)
-        (liberty? board color (inc x) y)
-        false))
-    (or
-      (and
-        (valid-pos? x (dec y))
-        (empty-field? board x (dec y)))
-      (if (same-color? board color x (dec y))
-        (liberty? board color x (dec y))
-        false))
-    (or
-      (and
-        (valid-pos? x (inc y))
-        (empty-field? board x (inc y)))
-      (if (same-color? board color x (inc y))
-        (liberty? board color x (inc y))
-        false))))
+  (liber? board '() color x y))
 
 (defn no-liberty? [board color x y]
-  (not (liberty? board color x y)))
+  (and
+    (same-color? board color x y)
+    (not (liberty? board color x y))))
 
 (defn suicide? [board color x y]
   (and
     (or
       (not-valid-pos? (dec x) y)
-      (if (same-color? color (dec x) y)
-        (no-liberty? (put-piece board color x y) color x y)
-        (liberty? (put-piece board color x y) color (dec x) y)))
+      (and
+        (occupied? board (dec x) y)
+        (if (same-color? board color (dec x) y)
+          (no-liberty? (put-piece board color x y) color x y)
+          (liberty? (put-piece board color x y) color (dec x) y))))
     (or
       (not-valid-pos? (inc x) y)
-      (if (same-color? color (inc x) y)
-        (no-liberty? (put-piece board color x y) color x y)
-        (liberty? (put-piece board color x y) color (inc x) y)))
+      (and
+        (occupied? board (inc x) y)
+        (if (same-color? board color (inc x) y)
+          (no-liberty? (put-piece board color x y) color x y)
+          (liberty? (put-piece board color x y) color (inc x) y))))
     (or
       (not-valid-pos? x (dec y))
-      (if (same-color? color x (dec y))
-        (no-liberty? (put-piece board color x y) color x y)
-        (liberty? (put-piece board color x y) color x (dec y))))
+      (and
+        (occupied? board x (dec y))
+        (if (same-color? board color x (dec y))
+          (no-liberty? (put-piece board color x y) color x y)
+          (liberty? (put-piece board color x y) color x (dec y)))))
     (or
       (not-valid-pos? x (inc y))
-      (if (same-color? color x (inc y))
-        (no-liberty? (put-piece board color x y) color x y)
-        (liberty? (put-piece board color x y) color x (inc y))))))
+      (and
+        (occupied? board x (inc y))
+        (if (same-color? board color x (inc y))
+          (no-liberty? (put-piece board color x y) color x y)
+          (liberty? (put-piece board color x y) color x (inc y)))))))
 
 (defn not-suicide? [board color x y]
   (not (suicide? board color x y)))
 
 (defn suicide-with-benefits? [board color x y]
   (and
-    (valid-pos? board x y)
+    (valid-pos? x y)
     (empty-field? board x y)
     (or
       (not-suicide? board color x y)
-      (and
-        (no-liberty? board (opposite color) (dec x) y)
-        (no-liberty? board (opposite color) x (dec y))
-        (no-liberty? board (opposite color) (inc x) y)
-        (no-liberty? board (opposite color) x (inc y))))))
+      (or
+        (no-liberty?  (put-piece board color x y) (opposite color) (dec x) y)
+        (no-liberty?  (put-piece board color x y) (opposite color) x (dec y))
+        (no-liberty?  (put-piece board color x y) (opposite color) (inc x) y)
+        (no-liberty?  (put-piece board color x y) (opposite color) x (inc y))))))
   
 (defn remove-struct-of-color [board color x y]
-  (if (same-color? board color x y)
+  (if (and
+        (valid-pos? x y)
+        (same-color? board color x y))
     (->
       (remove-piece board x y)
       (remove-struct-of-color color (inc x) y)
       (remove-struct-of-color color x (inc y))
       (remove-struct-of-color color (dec x) y)
-      (remove-struct-of-color color x (dec y)))))
+      (remove-struct-of-color color x (dec y)))
+    board))
 
 (defn remove-if-no-liberty [board color x y]
   (if (and 
-        (no-liberty? board color x y)
-        (same-color? board color x y))
+        (occupied? board x y)
+        (no-liberty? board color x y))
     (remove-struct-of-color board color x y)
     board))
 
@@ -143,4 +167,4 @@
       (remove-if-no-liberty (opposite color) x (dec y))
       (remove-if-no-liberty (opposite color) (inc x) y)
       (remove-if-no-liberty (opposite color) x (inc y)))
-    (put-piece board color x y)))
+    board))
